7cfc4d58f7343d89e022eb4668708f8d25b4ad80
unknown <Arian>
2024-09-03 17:09:29 -0300

    Agregamos algunos cambios de gráficos y modelo de ventana

--- Lazos de seguimiento/ModelRO.m
+++ Lazos de seguimiento/ModelRO.m
@@ -26,12 +26,19 @@ N = @(h) 400 * exp(-h/8e3); % Refractividad modelo A (exponencial)
 % Por lo que "y" es el avance de la onda y "z" representa el movimiento en vertical
 
 excess_phase = zeros(length(z_values), num_screens);
-
 for i = 1:num_screens
     h = z_values + (i - 1) * delta_l; % Altura correspondiente a la pantalla
     refractivity = N(h); % Refractividad en esa altura
     excess_phase(:, i) = 1e-6 * refractivity * delta_l; % Fase excedente
 end
+
+% Parámetros del modelo de atenuación
+alpha_0 = 1e-4; % Coeficiente de atenuación inicial (valor de ejemplo)
+H_0 = 4e3; % Escala de altura (ejemplo para la troposfera húmeda)
+
+% Modelo de atenuación atmosférica
+attenuation_factor = exp(-alpha_0 * exp(-z_values / H_0));
+
 
 % Inicializa la señal de entrada (asumiendo una señal plana de entrada)
 U_in = ones(size(z_values));
@@ -39,7 +46,7 @@ U_in = ones(size(z_values));
 % Propaga la señal a través de las pantallas
 for i = 1:num_screens
     % Calcula la amplitud compleja de salida en la pantalla actual (un instante luego)
-    U_out = U_in .* exp(1i * k * excess_phase(:, i));
+    U_out = U_in .* exp(1i * k * excess_phase(:, i)).* attenuation_factor';
     
     % Realiza la expansión en Fourier (acá hago la suma tal cual el paper)
     U_out_fft = fft(U_out);
@@ -96,15 +103,10 @@ grid on;
 
 
 % Cálculo de aceleración de fase
-
 delta_t = 1/50; % Lo tomamos del paper
-
 for i = 2:length(phase_LEO)-1
-
-        diff_phase(i) = (phase_LEO(i+1)-2*phase_LEO(i)+phase_LEO(i-1))/(2*pi*delta_t^2);
-
+    diff_phase(i) = (phase_LEO(i+1)-2*phase_LEO(i)+phase_LEO(i-1))/(2*pi*delta_t^2);
 end
-
 figure;
 plot(z_values(2:end),diff_phase);
 title("Aceleración de fase")
--- Lazos de seguimiento/Sig_sintetica_RO.m
+++
@@ -1,8 +0,0 @@
-% Generación de señal sintetica RO 
-
-% Deberíamos tener en cuenta 
-% Retardo integral para cada muestra dependiendo la elevación 
-% Disminución en la amplitud 
-
-% Para simular el problema primero tenemos que definir que modelos me van a aportar la fase en cuestión
-
